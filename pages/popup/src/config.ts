43058ca1185296776a8f5451368e76e3546a6e01
Sourav Mandal <sm227465@gmail.com>
2025-03-08 19:33:23 +0530

    fix

--- pages/popup/src/config.ts
+++ pages/popup/src/config.ts
@@ -4,6 +4,6 @@ export const config = {
     BASE_API: 'https://api.torbox.app',
     VERSION: 'v1',
     PATH: 'api/torrents/createtorrent',
-    KEY: '8a9dbe6f-7f15-4cb4-8123-6b44203a2a72',
+    KEY: '',
   },
 };
