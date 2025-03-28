ac11bcc233047e3d21fe5109fe53245dfa0afcc4
Sourav Mandal <sourav.mandal@vyrazu.com>
2025-03-28 12:02:11 +0530

    Readme update

--- pages/popup/src/data/supported-sp.ts
+++ pages/popup/src/data/supported-sp.ts
@@ -2,6 +2,7 @@ export const SUPPORTED_CTSP = [
   { name: 'RealDebrid', value: 'real-debrid', isAvailable: true },
   { name: 'TorBox', value: 'torbox', isAvailable: true },
   { name: 'Seedr', value: 'seedr', isAvailable: true },
+  { name: 'AllDebrid', value: 'all-debrid', isAvailable: false },
   { name: 'BitPort', value: 'bitport', isAvailable: false },
   { name: 'Offcloud', value: 'offcloud', isAvailable: false },
   { name: 'Put.io', value: 'putio', isAvailable: false },
